fb00c1359affa40e72174b306dd68b2063acc6a6
Andreas Åkerlöf <andreas@akerlof.se>
2019-05-22 17:21:05 +0200

    Tagit bort gammal kod

--- src/classes/User.php
+++ src/classes/User.php
@@ -1,14 +1,6 @@
 <?php
 
 class User extends Mapper {
-  // public function getUserByID($userID) {
-  //   $statement = $this->db->prepare("SELECT * FROM users WHERE userID = :userID");
-  //   $statement->execute([
-  //     ':userID' => $userID
-  //   ]);
-  //   return $statement->fetch(PDO::FETCH_ASSOC);
-  // }
-
   // Hämta alla användare
   public function getAllUsers() {
     $statement = $this->db->prepare('SELECT userID, username FROM users');
