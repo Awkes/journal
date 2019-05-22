5d567983dde66d8d09b6114da106e439dbf1c035
Magnus <magnus.lidmyr@gmail.com>
2019-05-22 16:31:37 +0200

    route documentation for comments

--- src/settings.php
+++ src/settings.php
@@ -12,11 +12,11 @@ return [
 
     // Database settings
     'db' => [
-      'host' => '127.0.0.1',
-      /* 'host' => 'localhost:8889', */
+      /* 'host' => '127.0.0.1', */
+      'host' => 'localhost:8889',
       'user' => 'root',
-      'pass' => '',
-      // 'pass' => 'root',
+      /* 'pass' => '', */
+      'pass' => 'root',
       'dbname' => 'journal'
     ]
   ],
